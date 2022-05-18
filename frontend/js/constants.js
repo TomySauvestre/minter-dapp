f652e92ee75350077fccda4e9b91713c19c28421
Tomy <tomy.sauvestre@univ-tlse3.fr>
2022-05-18 17:51:29 +0200

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x5a69A3dd2B5E41186f8594d842B44E4F7dA2DF76"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
