b85512d29dbf9590fe16b730b8bbe1d78e4a15cd
Tomy <tomy.sauvestre@univ-tlse3.fr>
2022-06-23 12:40:43 +0200

    e

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -573,10 +573,10 @@ allCross.forEach(element => {
     const currentChoice = this.parentNode.parentNode.childNodes[3];
     if (this.src.includes('plus')){
       this.src = '../images/x-icon/less.png';
-      gsap.to(currentChoice, {duration: 0.2, height: height + 40, opacity: 1, padding: '1vw 1vw'})
+      gsap.to(currentChoice, {duration: 0.2, height: height + 40, opacity: 1, padding: '20px 15px'})
     } else if (this.src.includes('less')){
       this.src = '../images/x-icon/plus.png';
-      gsap.to(currentChoice, {duration: 0.2, height: 0, opacity: 0, padding: '0vw 1vw'})
+      gsap.to(currentChoice, {duration: 0.2, height: 0, opacity: 0, padding: '0px 15px'})
     }
 
   })
