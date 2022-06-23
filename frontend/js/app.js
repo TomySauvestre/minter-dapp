0a9c758287f988469658986059c4b1bb5e5ec479
Tomy <tomy.sauvestre@univ-tlse3.fr>
2022-06-23 11:33:19 +0200

    e

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -430,7 +430,7 @@ function change() {
   direction = -1;
   carousel.style.justifyContent = 'flex-start';
   slider.style.transform = 'translate(-20%)';
-  setTimeout(change, 2000);
+  setTimeout(change, 3600);
 }
 
 next.addEventListener('click', function() {
