a14a72b8d10570d69816fcfb9dd60fa3e82e5575
Tomy <tomy.sauvestre@univ-tlse3.fr>
2022-07-02 12:47:51 +0200

    e

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,5 @@
 let accounts;
-/*
+/*MODIFIER */
 // METAMASK CONNECTION
 window.addEventListener("DOMContentLoaded", async () => {
   //const welcomeH1 = document.getElementById("welcomeH1");
@@ -368,7 +368,7 @@ async function mint() {
   }
 }
 
-*/
+   /* MODIFIER*/
 
 var img = [];
 img[0] = "../images/x-icon/img1.png";
@@ -460,7 +460,7 @@ arrow.addEventListener("mouseout", ()=> {
 })
 
 
-/*
+/* MODIFER */
 const opensea = document.querySelector('#opensea');
 
 opensea.addEventListener("mouseenter", ()=> {
@@ -472,7 +472,7 @@ opensea.addEventListener("mouseout", ()=> {
   opensea.src = "../images/header/opensea.png";
   opensea.style.width = "2vw";
 })
-*/
+/*MODIFIER*/
 
 
 
@@ -504,7 +504,7 @@ discord.addEventListener("mouseout", ()=> {
   discord.style.width = "2vw";
 })
 
-/*
+/* MODIIFER */
 const wallet_btn = document.querySelector('.wallet-btn');
 
 wallet_btn.addEventListener("mouseenter", () => {
@@ -515,7 +515,7 @@ wallet_btn.addEventListener("mouseenter", () => {
 wallet_btn.addEventListener("mouseout", () => {
   wallet_btn.style.color = "#9d0012";
   wallet_btn.style.background  = "white";
-})*/
+}) /* MODIFIER*/
 
 
 const jDiscord = document.querySelector('.jDiscord');
@@ -542,7 +542,7 @@ jTwitter.addEventListener("mouseout", () => {
   jTwitter.style.background  = "white";
 })
 
-/*
+/* MODIFIER */
 const actionBTN = document.querySelector('#actionButton');
 
 actionBTN.addEventListener("mouseenter", () => {
@@ -565,7 +565,7 @@ mintBTN.addEventListener("mouseenter", () => {
 mintBTN.addEventListener("mouseout", () => {
   mintBTN.style.color = "#9d0012";
   mintBTN.style.background  = "white";
-})*/
+})    /*MODIFIER*/
 
 
 const allCross = document.querySelectorAll('.visible-panel img');
